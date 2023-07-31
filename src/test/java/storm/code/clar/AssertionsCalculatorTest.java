3b372a7748fca895a0343bfee0b5d62253c36222
asepsaputra <v.asep.saputra@adira.co.id>
2023-07-31 14:08:55 +0700

    Add AssertionsCalculatorTest & AssumptionsCalculatorTest

--- src/test/java/storm/code/clar/AssertionsCalculatorTest.java
+++ src/test/java/storm/code/clar/AssertionsCalculatorTest.java
@@ -8,21 +8,19 @@ import storm.code.generator.NameGenerator;
  **/
 
 @DisplayNameGeneration(NameGenerator.class)
-public class CalculatorTest {
-
+class AssertionsCalculatorTest {
     private final Calculator calculator = new Calculator();
 
     @Test
-    public void test_divide_success() {
+    void test_divide_success() {
         int actual = calculator.divide(2,1);
         int expected = 2;
-        Assumptions.assumeTrue(actual != expected);
+        Assertions.assertEquals(expected, actual);
     }
 
     @Test
-    @Disabled
-    public void test_divide_failed() {
-        Assertions.assertThrows(ArithmeticException.class, ()-> {
+    void test_divide_failed() {
+        Assertions.assertThrows(ArithmeticException.class, () -> {
             calculator.divide(2, 0);
         }, "Can't divide by zero!");
     }
---
+++ src/test/java/storm/code/clar/AssumptionsCalculatorTest.java
@@ -0,0 +1,35 @@
+package storm.code.clar;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Assumptions;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.Test;
+import storm.code.generator.NameGenerator;
+
+/**
+ * @author Asep Saputra
+ **/
+
+@DisplayNameGeneration(NameGenerator.class)
+public class AssumptionsCalculatorTest {
+    private final Calculator calculator = new Calculator();
+
+    @Test
+    void test_divide_success() {
+        int actual = calculator.divide(2,1);
+        int expected = 2;
+        Assumptions.assumeTrue(expected != actual);
+    }
+
+    @Test
+    void test_divide_failed() {
+        int numerator = 2;
+        int denominator = 0;
+        Assumptions.assumeTrue(denominator != 0);
+
+        Assertions.assertThrows(ArithmeticException.class, () -> {
+            calculator.divide(numerator, denominator);
+        }, "Can't divide by zero!");
+    }
+
+}
